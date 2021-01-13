a351671dfec980737b430b610d70eeb4327c3670
chaddavidcheney <76493227+chaddavidcheney@users.noreply.github.com>
2021-01-13 00:43:27 -0600

    Init commit

---
+++ src/webparts/mockdownify/components/IMockdownifyProps.ts
@@ -0,0 +1,3 @@
+export interface IMockdownifyProps {
+  description: string;
+}
---
+++ src/webparts/mockdownify/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMockdownifyWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MockdownifyWebPartStrings' {
+  const strings: IMockdownifyWebPartStrings;
+  export = strings;
+}
